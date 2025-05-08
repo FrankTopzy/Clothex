9df44686725eab8183e7d7c4bda7377d77e0f7bd
Frank <temitopeadeoye787898@gmail.com>
2025-05-08 13:50:41 +0100

    search section added

--- Clothex/src/Components/Footer/Footer.jsx
+++ Clothex/src/Components/Footer/Footer.jsx
@@ -1,10 +1,61 @@
 import React from 'react'
 import './Footer.css'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div className='footer'>
-      <h3>This is the Footer</h3>
+    <div className='footer p-5'>
+      <div className='footer-container bg-white uppercase text-black'>
+        <div className='footer-upper text-[12px]'>
+          <div className='max-w-[500px]'>
+            <h1>get 10% off your next order</h1>
+
+            <form action="" className='w-full'>
+              <input type="text" placeholder='NAME' />
+              <input type="email" placeholder="EMAIL" />
+              <button onClick={(e) => e.preventDefault()} className='bg-black text-white py-2 px-4 w-full mb-[30px] mt-2'>SUBSCRIBE TO OUR NEWSLETTER</button>
+            </form>
+            <p>*BY SIGNING UP, YOU AGREE TO RECEIVE EMAILS ABOUT Clothex AND OUR OTHER TERMS.</p>
+          </div>
+
+          <div>
+            <h1>Shop here</h1>
+
+            <p>All Items</p>
+            <p>Male</p>
+            <p>Female</p>
+            <p>Accessories</p>
+            <p>Collections</p>
+          </div>
+
+          <div>
+            <h1>Quick Links</h1>
+
+            <p>About us</p>
+            <p>OUR WALK-IN STORE</p>
+            <p>Faqs</p>
+            <p>Policies</p>
+            <p>Your account</p>
+          </div>
+
+          <div className='contact-us'>
+            <h1>Contact</h1>
+
+            <p>Shop 38/39, Aaron’s Lekki Mall, Admiralty way, Lagos, Nigeria.</p>
+            <p>Operating hours: Monday – Saturday (10am -10pm) Sunday (12pm-10pm)</p>
+            <p>+2349131058119</p>
+
+            <div className='scoial-media'>
+              <Link to="/">Clothex</Link>
+            </div>
+          </div>
+        </div>
+
+        <div className='display flex justify-between items-center py-[40px] px-[100px]'>
+          <p className='text-[12px]'>© 2025 CLothex by FRANK All Rights Reserved</p>
+          <Link to={"/"} className='text-2xl font-semibold'>CLOTHEX</Link>
+        </div>
+      </div>
     </div>
   )
 }
--- Clothex/src/Pages/Home/Home.jsx
+++ Clothex/src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { use, useContext, useEffect } from 'react'
+import React, { use, useContext, useEffect, useState } from 'react'
 import './Home.css'
 import homeBanner from '../../assets/bg-img/bg-1.jpg'
 import { Link } from 'react-router-dom'
@@ -11,8 +11,25 @@ import { baseCurrency } from '../../Services/formatCurrency'
 import Cart from '../../Components/Cart/Cart'
 
 function Home() {
-  const {cartRef, cart, count,setCount, removeFromCart, currency} = useContext(ClothContext)
+  const {cartRef, cart, count,setCount, removeFromCart, currency, setClothes, clothes} = useContext(ClothContext)
   const [loading, setLoading] = React.useState(true);
+  const [search, setSearch] = useState("");
+  const [filteredSearch, setFilteredSearch] = useState([])
+
+  const handleSearch = useEffect((cloth) => {
+
+    //if (!search.trim()) return
+    //if (loading) return
+
+    //setLoading(true)
+
+    setFilteredSearch(products.filter((cloth) => {
+      return cloth.name.toLowerCase().includes(search.toLowerCase())
+    }))
+
+    console.log(filteredSearch)
+  }, [search])
+  
   
 
   return (
@@ -23,9 +40,37 @@ function Home() {
       <div className="hero">
         <img src={homeBanner} alt="" className='w-full'/>
 
-        <div className="search-container bg-white flex items-center gap-1 rounded-3xl w-[60%]">
-          <input type="search" className='search-input flex-1 py-3 pl-5 border-0 outline-none text-black' placeholder='Search Product.........'/>
-          <button className='w-[40px]'><img src={searchIcon} alt="" className='w-[40px] p-2'/></button>
+        <div className="search-container w-[30%]">
+          <div className='bg-white flex items-center gap-1 rounded-3xl'>
+            <input type="search" className='search-input flex-1 py-3 pl-5 border-0 outline-none text-black' placeholder='Search Product.........' onChange={(e) => setSearch(e.target.value)} value={search}/>
+            <button className='w-[40px]' ><img src={searchIcon} alt="" className='w-[40px] p-2'/></button>
+          </div>
+          
+
+          {search.length > 0 &&
+            filteredSearch.slice(0, 3).map((cloth) => {
+              let clothPrice = cloth.price;
+              //console.log(cloth.id)
+          
+              if (currency.name === "ngn") {
+                clothPrice = cloth.price;
+              }
+              else if (currency.name === "usd") {
+                clothPrice = baseCurrency(clothPrice).toFixed()
+              }
+
+               return (
+                <div className="search-item-details bg-white text-black px-4 py-2 flex items-center justify-between gap-4 w-full my-4 text-left" key={cloth.id}>
+                <img src={cloth.image} alt="image" width="80px"/>
+    
+                <div className='cart-info flex-1'>
+                  <p>{cloth.name}</p>
+                  <p className='my-2'>{currency.symbol} {((clothPrice)).toLocaleString()}</p>
+                </div>
+              </div>
+              )
+            })
+          }
         </div>
 
         <Link className='absolute bottom-4 '>EXPLORE OUR NEW COLLECTION</Link>
