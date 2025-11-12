0970e08ce27b2dc408c78848b6ff91eca715e7f8
Frank <temitopeadeoye787898@gmail.com>
2025-11-12 14:04:43 +0100

    Moved all files to the parent folder for easy deployment.

--- src/Components/Context/Context.jsx
+++ src/Components/Context/Context.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, use, useEffect, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 export const ClothContext = createContext();
 
