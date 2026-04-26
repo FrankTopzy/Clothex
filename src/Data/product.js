8dccf4a033a7b5f2d276c54e3f764151b617351a
Frank <temitopeadeoye787898@gmail.com>
2026-04-26 12:08:20 +0100

    corrected the image path.

--- src/Data/product.js
+++ src/Data/product.js
@@ -1,14 +1,14 @@
 export const products = [{
   id: "1hf",
   name: "HF FLOWER SPANDEX WHITE TEE",
-  image: "../../assets/Clothes/hf/HF FLOWER SPANDEX WHITE TEE.jpg",
+  image: "src/assets/Clothes/hf/HF FLOWER SPANDEX WHITE TEE.jpg",
   imageAlt: "/src/assets/Clothes/hf/HF%20FLOWER%20SPANDEX%20WHITE%20TEE.jpg",
   price: 240000,
   quantity: 10,
 }, {
   id: "2hf",
   name: "HF Football Jersey- Green",
-  image: "../../assets/Clothes/hf/HF Football Jersey- Green.jpg",
+  image: "src/assets/Clothes/hf/HF Football Jersey- Green.jpg",
   price: 320000,
   quantity: 10,
 }, {
