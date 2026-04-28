ba17b35e2907644a430373ccbce6b0de2eff662e
Frank <temitopeadeoye787898@gmail.com>
2026-04-28 23:59:07 +0100

    Designed the page.

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Payment() {
   return (
-    <div>
-      
+    <div className='min-h-screen bg-white pt-30 text-black flex items-center justify-center'>
+      <p className='text-2xl bg-gray-300 w-100 h-50 justify-center flex items-center animate-bounce'>Under Construction!</p>
     </div>
   )
 }
